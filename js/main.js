5a912b113a5dc49966d2692a7d2d5f1938097162
plusmasa <maksim.shuster@plusmasa.com>
2014-12-23 10:22:27 -0800

    Added in the jquery from the finished project. Changed .selected-client to .active-client to match the new jquery.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
 	smoothScroll(300);
 	workLoad();
 	workBelt();
@@ -49,42 +49,46 @@ function workBelt() {
 }
 
 function clientStuff() {
-	$('.client-unit').hide();
-	$('.client-unit').first().show();
-	$('.client-logo').first().addClass('selected-client');
+  
+  $('.client-unit').first().addClass('active-client');
+  $('.clients-logos').children().first().addClass('active-client');
+  $('.clients-mobile-nav span').first().addClass('active-client');
+  
+  
+  $('.client-logo, .clients-mobile-nav span').click(function() {
+    var $this = $(this),
+        $siblings = $this.parent().children(),
+        position = $siblings.index($this);
+        
+    $('.client-unit').removeClass('active-client').eq(position).addClass('active-client');
+    $siblings.removeClass('active-client');
+    $this.addClass('active-client');
+  });
+  
+  /*
+  $('.client-control-next, .client-control-prev').click(function() {
+  
+    var $this = $(this),
+        curActiveClient = $('.clients-belt').find('.active-client'),
+        position = $('.clients-belt').children().index(curActiveClient),
+        clientNum = $('.client-unit').length;
+        
+      if($this.hasClass('client-control-next')) {
+        
+        if(position < clientNum -1){
+          $('.active-client').removeClass('active-client').next().addClass('active-client');
+        } else {
+          $('.client-unit').removeClass('active-client').first().addClass('active-client');
+          $('.client-logo').removeClass('active-client').first().addClass('active-client');
+        }
+        
+      } else {
+        
+        if (position === 0) {
+          $('.client-unit').removeClass('active-client').last().addClass('active-client');
+          $('.client-logo').removeClass('active-client').last().addClass('active-client');
+        } else {
+          $('.active-client').removeClass('active-client').prev().addClass('active-client');  
+        }
 
-	$('.client-logo').click(function(){
-		var $this = $(this);
-		var $siblings = $this.parent().children();
-		var position = $siblings.index($this);
-		$('.client-unit').fadeOut('fast').eq(position).fadeIn('slow');
-		$siblings.removeClass('selected-client');
-		$this.addClass('selected-client');
-	});
-
-	$('.client-control-prev, .client-control-next').click(function() {
-		var $this = $(this);
-		var clients = $('.client-unit').siblings();
-		var position = $('.selected-client').index();
-
-		if ($this.hasClass('client-control-next')) {
-			if(position < clients.length-1){
-				position = position + 1;
-			} else {
-				position = 0;
-			}
-		}
-
-		if ($this.hasClass('client-control-prev')) {
-			if(position > 0){
-				position = position - 1;
-			} else {
-				position = clients.length-1;
-			}
-		}
-
-		$('.client-unit').fadeOut('fast').eq(position).fadeIn('slow');
-		$('.clients-logos').children().removeClass('selected-client');
-		$('.client-logo').eq(position).addClass('selected-client');
-	});
-}
+      } */
